5c895c99d0e9853fbbf1d34dda837999e30499ac
SYAAINN <alswo08319@gmail.com>
2025-05-28 15:18:39 +0900

    init : presentation layer 기초 세팅

---
+++ app/src/main/java/com/syaainn/dailic/presentation/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.syaainn.dailic.presentation.ui
+
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.syaainn.dailic.ui.theme.DAILICTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        enableEdgeToEdge()
+        setContent {
+            DAILICTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/syaainn/dailic/presentation/ui/MainNavigator.kt
@@ -0,0 +1,4 @@
+package com.syaainn.dailic.presentation.ui
+
+class MainNavigator {
+}
