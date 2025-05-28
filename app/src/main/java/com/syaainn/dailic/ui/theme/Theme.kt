3810719966490ece55208f318406435413d9c5a1
SYAAINN <alswo08319@gmail.com>
2025-05-28 15:18:09 +0900

    init : color, font 세팅

--- app/src/main/java/com/syaainn/dailic/ui/theme/Theme.kt
+++ app/src/main/java/com/syaainn/dailic/ui/theme/Theme.kt
@@ -9,50 +9,59 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+object GongBaekTheme {
+    val colors: DailicColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalDailicColors.current
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+    val typography: DailicTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalDailicTypography.current
+}
 
 @Composable
-fun DailicTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideGongBaekColorsAndTypography(
+    colors: DailicColors,
+    typography: DailicTypography,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    CompositionLocalProvider(
+        LocalDailicColors provides colors,
+        LocalDailicTypography provides typography,
+        content = content
+    )
+}
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+@Composable
+fun DAILICTheme(
+    content: @Composable () -> Unit
+) {
+    ProvideGongBaekColorsAndTypography(
+        colors = defaultDailicColors,
+        typography = defaultDailicTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    statusBarColor = white.toArgb()
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = true
+                }
+            }
+        }
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }
