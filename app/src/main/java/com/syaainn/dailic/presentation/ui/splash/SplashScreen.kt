add2515fc748c3375dd536b41c2e470d05973c73
SYAAINN <alswo08319@gmail.com>
2025-06-12 11:43:22 +0900

    feat : splash screen

---
+++ app/src/main/java/com/syaainn/dailic/presentation/ui/splash/SplashScreen.kt
@@ -0,0 +1,68 @@
+package com.syaainn.dailic.presentation.ui.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.syaainn.dailic.R
+import com.syaainn.dailic.ui.theme.DAILICTheme
+import com.syaainn.dailic.ui.theme.DailicTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashRoute(
+    navigateToOccupation: () -> Unit
+) {
+    val systemUiController = rememberSystemUiController()
+    val backgroundColor = DailicTheme.colors.primaryBeige1
+
+    LaunchedEffect(Unit) {
+        systemUiController.setStatusBarColor(color = backgroundColor)
+        delay(2000)
+        navigateToOccupation()
+    }
+
+    SplashScreen()
+}
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DailicTheme.colors.primaryBeige1),
+        contentAlignment = Alignment.Center,
+        content = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.img_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = "꾸준히 학습하는 내 손 안의 자격증, 데일릭",
+                    color = DailicTheme.colors.gray800,
+                    style = DailicTheme.typography.body2Medium
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSplashScreen() {
+    DAILICTheme {
+        SplashScreen()
+    }
+}
