61f57cffc5f15ab4e18b92f4454d40d9409db60a
SYAAINN <alswo08319@gmail.com>
2025-08-10 21:34:49 +0900

    chore : splash screen change

--- app/src/main/java/com/syaainn/dailic/presentation/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/syaainn/dailic/presentation/ui/splash/SplashScreen.kt
@@ -1,10 +1,10 @@
 package com.syaainn.dailic.presentation.ui.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.syaainn.dailic.R
 import com.syaainn.dailic.ui.theme.DAILICTheme
@@ -37,31 +38,26 @@ fun SplashRoute(
 @Composable
 fun SplashScreen() {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(DailicTheme.colors.primaryBeige1),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
         content = {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.img_logo),
-                    contentDescription = null
-                )
-                Text(
-                    text = "꾸준히 학습하는 내 손 안의 자격증, 데일릭",
-                    color = DailicTheme.colors.gray800,
-                    style = DailicTheme.typography.body2Medium
-                )
-            }
+            Image(
+                painter = painterResource(id = R.drawable.img_logo),
+                contentDescription = null
+            )
+            Text(
+                text = "꾸준히 학습하는 내 손 안의 자격증, 데일릭",
+                modifier = Modifier.padding(top = 150.dp),
+                color = DailicTheme.colors.gray800,
+                style = DailicTheme.typography.body2Medium
+            )
         }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewSplashScreen() {
+private fun PreviewSplashScreen() {
     DAILICTheme {
         SplashScreen()
     }
