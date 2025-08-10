60040a99d0019c9d2cfcbaba8c5333768c45917a
SYAAINN <alswo08319@gmail.com>
2025-08-10 21:32:48 +0900

    feat : daily study screen

---
+++ app/src/main/java/com/syaainn/dailic/presentation/ui/dailystudy/DailyStudyContract.kt
@@ -0,0 +1,39 @@
+package com.syaainn.dailic.presentation.ui.dailystudy
+
+import com.syaainn.dailic.data.dummy.DailyStudyDummy
+import com.syaainn.dailic.presentation.model.Question
+import com.syaainn.dailic.presentation.util.base.UiEvent
+import com.syaainn.dailic.presentation.util.base.UiSideEffect
+import com.syaainn.dailic.presentation.util.base.UiState
+
+class DailyStudyContract {
+    data class State(
+        val currentQuestionNum: Int = 1,
+        val totalQuestionNum: Int = 20,
+        val todayQuestions: List<Question> = DailyStudyDummy.questionList,
+        val userAnswerList: IntArray = IntArray(totalQuestionNum) { 0 },
+        val showExitDialog: Boolean = false,
+        val showSubmitDialog: Boolean = false,
+    ): UiState {
+        val progress: Float = userAnswerList.count { it != 0 }.toFloat() / totalQuestionNum
+        val progressWeight = progress.coerceIn(0.0001f, 0.9999f)
+    }
+
+    sealed class Event: UiEvent {
+        data object OnBackClick: Event()
+        data object OnDismissExitDialog: Event()
+        data object OnConfirmExitDialog: Event()
+        data object OnScrapClick: Event()
+        data class OnAnswerClick(val optionNum: Int): Event()
+        data object OnPreviousQuestionClick: Event()
+        data object OnNextQuestionClick: Event()
+        data object OnSubmitClick: Event()
+        data object OnDismissSubmitDialog: Event()
+        data object OnConfirmSubmitDialog: Event()
+    }
+
+    sealed interface SideEffect: UiSideEffect {
+        data object NavigateToBack: SideEffect
+        data class NavigateToScore(val score: List<Boolean>): SideEffect
+    }
+}
