991d666aa5bcb1e674d6cb2f05585b961c0a8657
SYAAINN <alswo08319@gmail.com>
2025-11-27 13:22:01 +0900

    feat : 오답노트,스크랩 dto 수정

--- app/src/main/java/com/syaainn/dailic/data/dto/response/GetWrongProblemResponseDto.kt
+++ app/src/main/java/com/syaainn/dailic/data/dto/response/GetWrongProblemResponseDto.kt
@@ -11,7 +11,7 @@ data class GetWrongProblemResponseDto(
     @SerialName("questionText") val questionText: String,
     @SerialName("isCorrect") val isCorrect: Boolean,
     @SerialName("isScraped") val isScraped: Boolean,
-    @SerialName("userAnswer") val userAnswer: String,
+    @SerialName("userAnswer") val userAnswer: String?,
     @SerialName("isRetried") val isRetried: Boolean,
     @SerialName("answeredAt") val answerAt: String
 )
